4ed33a66c3cea5756e832fa56775cba98f193c02
coder-pig <2219376987@qq.com>
2020-01-09 10:58:32 +0800

    获得用户信息

--- src/main/java/com/ecut/design/Repository/BannerRepository.java
+++ src/main/java/com/ecut/design/Repository/BannerRepository.java
@@ -2,14 +2,18 @@ package com.ecut.design.Repository;
 
 import com.ecut.design.Model.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface BannerRepository extends JpaRepository<Banner,Long> {
 
+    @Modifying
+    @Transactional
     @Query(value = "delete from banner where id in(:ids)",nativeQuery = true)
     void deleteBannerByIds(Long[] ids);
 
--- src/main/java/com/ecut/design/Repository/CouponsRepository.java
+++ src/main/java/com/ecut/design/Repository/CouponsRepository.java
@@ -2,12 +2,16 @@ package com.ecut.design.Repository;
 
 import com.ecut.design.Model.Coupons;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CouponsRepository extends JpaRepository<Coupons,Long> {
 
+    @Modifying
+    @Transactional
     @Query(value = "delete from coupons where id in(:ids)",nativeQuery = true)
-    public  void deleteByIds(Long[] ids);
+    void deleteByIds(Long[] ids);
 }
