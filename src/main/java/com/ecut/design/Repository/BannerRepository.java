379d28d050fba758f102000516a229f0d11d8fd5
coder-pig <2219376987@qq.com>
2020-01-09 17:54:25 +0800

    提交

--- src/main/java/com/ecut/design/Repository/BannerRepository.java
+++ src/main/java/com/ecut/design/Repository/BannerRepository.java
@@ -4,6 +4,7 @@ import com.ecut.design.Model.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +15,8 @@ public interface BannerRepository extends JpaRepository<Banner,Long> {
 
     @Modifying
     @Transactional
-    @Query(value = "delete from banner where id in(:ids)",nativeQuery = true)
-    void deleteBannerByIds(Long[] ids);
+    @Query(value = "delete from banner where id in (:ids)",nativeQuery = true)
+    void deleteBannersByIds(@Param ("ids") Long[] ids);
 
     @Query(value = "select * from banner where status='展示中' ",nativeQuery = true)
     List<Banner> findShowBanners();
