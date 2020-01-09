cf1335cf6d56d49b4dea83ee0bd4598788162be9
coder-pig <2219376987@qq.com>
2020-01-09 10:58:53 +0800

    获得用户信息

--- src/main/java/com/ecut/design/Repository/UserRepository.java
+++ src/main/java/com/ecut/design/Repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.ecut.design.Repository;
 
 import com.ecut.design.Model.User;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
